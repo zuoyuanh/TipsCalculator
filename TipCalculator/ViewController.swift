a7372c55c75d84f25515f0c65a50a022cb9928e0
zuoyuanh <ipad2@msn.com>
2017-05-05 22:52:07 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Zuoyuan Huang on 5/5/17.
+//  Copyright © 2017 Zuoyuan Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

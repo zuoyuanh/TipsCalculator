f133ef0d5eaae763ecef1f797c1b8214ddfa1dba
zuoyuanh <David@Davids-MacBook-Pro-2.local>
2017-05-08 22:10:55 -0700

    first commit

---
+++ TipCalculator/TipsCalculatorConfigurations.swift
@@ -0,0 +1,13 @@
+//
+//  TipsCalculatorConfigurations.swift
+//  TipCalculator
+//
+//  Created by Zuoyuan Huang on 5/8/17.
+//  Copyright © 2017 Zuoyuan Huang. All rights reserved.
+//
+
+import UIKit
+
+class TipsCalculatorConfigurations: NSObject {
+
+}
